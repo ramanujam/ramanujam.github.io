b365e3e9503d4f975c752219fe04de003bd17eca
Rohan Singh <rohans1@andrew.cmu.edu>
2018-11-30 00:58:29 -0800

    add parts working

--- js/main.js
+++ js/main.js
@@ -63,3 +63,9 @@ if ($(this).scrollTop() > 350){
     $('header').removeClass("sticky");
   }
 });
+
+var touch = Modernizr.touch;
+$('.img-holder').imageScroll({
+  imageAttribute: (touch === true) ? 'image-mobile' : 'image',
+  touch: touch
+});
